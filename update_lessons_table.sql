83493f8c3b375a90600d338e4e8dd738aec007fe
Godsizee <badesebastian@outlook.com>
2026-05-08 13:05:37 +0200

    1

--- update_lessons_table.sql
+++
@@ -1,6 +0,0 @@
--- Spalte "type" hinzufügen
-ALTER TABLE lessons 
-ADD COLUMN type VARCHAR(20) DEFAULT 'article' CHECK (type IN ('article', 'quiz'));
-
--- Aktualisieren der existierenden Lektionen
-UPDATE lessons SET type = 'article' WHERE type IS NULL;
--- update_progress_score.sql
+++
@@ -1,2 +0,0 @@
--- update_progress_score.sql
-ALTER TABLE user_progress ADD COLUMN IF NOT EXISTS score INT DEFAULT NULL;
